82440dbf030c19f8d4f0ee57ce2cf394990171da
LuanKuhlmann <l.campos.kuhlmann@outlook.com>
2023-08-01 17:30:44 -0300

    Connected creditappraiser to gateway

--- mscloudgateway/src/main/java/io/github/luankuhlmann/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/luankuhlmann/mscloudgateway/MscloudgatewayApplication.java
@@ -21,6 +21,7 @@ public class MscloudgatewayApplication {
 				.routes()
 					.route(r -> r.path("/clients/**").uri("lb://msclients"))
 					.route(r -> r.path("/cards/**").uri("lb://mscards"))
+					.route(r -> r.path("/credit-appraiser/**").uri("lb://mscreditappraiser"))
 				.build();
 	}
 
