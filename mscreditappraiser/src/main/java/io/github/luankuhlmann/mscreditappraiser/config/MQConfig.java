e6e9f5ac83c87f405b7a524c7ffe680b217e23d1
LuanKuhlmann <l.campos.kuhlmann@outlook.com>
2023-08-07 09:10:20 -0300

    ENDPOINT Card Issuance Publisher

--- mscreditappraiser/src/main/java/io/github/luankuhlmann/mscreditappraiser/config/MQConfig.java
+++ mscreditappraiser/src/main/java/io/github/luankuhlmann/mscreditappraiser/config/MQConfig.java
@@ -2,6 +2,7 @@ package io.github.luankuhlmann.mscreditappraiser.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,6 +11,7 @@ public class MQConfig {
     @Value("${mq.queues.cards-issuance}")
     private String cardIssuanceRow;
 
+    @Bean
     public Queue queueCardIssuance() {
         return new Queue(cardIssuanceRow, true);
     }
